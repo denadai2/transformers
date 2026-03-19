983060905c23e7ea539249a8ac6bb68f82dc2e62
Marco De Nadai <mdenadai@spotify.com>
2026-03-19 15:23:04 +0100

    fixed ruff

--- src/transformers/models/qwen2_audio/modeling_qwen2_audio.py
+++ src/transformers/models/qwen2_audio/modeling_qwen2_audio.py
@@ -154,7 +154,8 @@ class Qwen2AudioAttention(nn.Module):
     ) -> tuple[torch.Tensor, torch.Tensor | None, tuple[torch.Tensor] | None]:
         """Input shape: Batch x Time x Channel"""
 
-        hidden_states = hidden_states.unsqueeze(-1)
+        if len(hidden_states.size()) == 2:
+            hidden_states = hidden_states.unsqueeze(-1)
 
         bsz, tgt_len, _ = hidden_states.size()
 
